6cdc4e22f4bb28fb06b7a7ebe1616325385056f6
Muralikrishna <kemamurali@gmail.com>
2019-06-28 01:13:16 +0530

    Update dongle.sh

--- dongle.sh
+++ dongle.sh
@@ -1,1 +1,18 @@
 #!/bin/sh
+yum install linux-headers-`uname -r` gcc g++ make libnewt-dev libncurses5-dev openssl libssl-dev zlib1g-dev
+
+wget https://github.com/bg111/asterisk-chan-dongle/archive/master.zip
+
+unzip master.zip
+cd asterisk-chan-dongle-master
+aclocal && autoconf && automake -a
+./configure
+./configure --disable-debug --enable-apps --enable-manager
+make
+make install
+mv /etc/dongle.conf  /etc/asterisk/
+
+module reload now
+
+module load chan_dongle.so
+
